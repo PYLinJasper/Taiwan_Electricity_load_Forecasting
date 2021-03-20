1f5953419d746cc9ac19840726b49eae230d75da
Rocky <enterniter@gmail.com>
2021-03-20 10:11:32 +0800

    New data Added

---
+++ app.py
@@ -0,0 +1,16 @@
+import argparse
+import pandas as pd
+import numpy as np
+import sklearn
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # training data import
+    parser.add_argument('--training', default='training_data.csv',
+    help='input training data file name')
+    # predict data export
+    parser.add_argument('--output', default='submission.csv',
+    help='output file name')
+    print("No problem ")
+
+    args = parser.parse_args()
